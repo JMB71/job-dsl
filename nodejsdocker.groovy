5e6e0f7f85b8bfeef5c4dc975da44050360260c6
JMB71 <jean-marie.bernabeu@laposte.net>
2021-03-23 18:59:35 +0100

    Create nodejsdocker.groovy

---
+++ nodejsdocker.groovy
@@ -0,0 +1,25 @@
+job('NodeJS Docker example') {
+  scm {
+    git('git://github.com/wardviaene/docker-demo.git') { node ->
+      node / gitConfigName('DSL User')
+      node / gitConfigEmail('jean-marie.bernabeu@laposte.net')
+    }
+  }
+  triggers {
+    scm('H/5 * * * *()
+  }
+  wrappers {
+    nodejs('nodejs-12.21.0')
+  }
+  steps {
+    dockerBuildAndPublish {
+      repositoryName('jmb25/docker-nodejs-demo')
+      tag('${GIT_REVISION,length=9}')
+      registryCredentials('dockerhub')
+      forcePull(false)
+      forceTag(false)
+      createFingerprints(false)
+      skipDecorate()
+    }
+  }
+}
